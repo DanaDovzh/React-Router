54eb946b047d9ed9eadef688db3015303c82aac9
DanaDovzh <danadovzh16@gmail.com>
2021-01-16 00:26:55 +0200

    Fix with links

--- src/components/PagesConstructor/Nav.js
+++ src/components/PagesConstructor/Nav.js
@@ -4,7 +4,7 @@ import { NavLink, BrowserRouter } from 'react-router-dom';
 export const Nav = () => {
     return (
         <div className='nav-pages'>
-                <NavLink to='./mainPage' className='nav-link' activeClassName="active">Main</NavLink>
+                <NavLink to='/mainPage' className='nav-link' activeClassName="active">Main</NavLink>
                 <NavLink to='/posts' className='nav-link' activeClassName="active">Posts</NavLink>
                 <NavLink to='/contacts' className='nav-link' activeClassName="active">Contacts</NavLink>
                 <NavLink to='/imagesPage' className='nav-link' activeClassName="active">Images</NavLink>

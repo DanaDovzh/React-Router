da5be136f8322716d8043f211fa93440a35cde21
DanaDovzh <danadovzh16@gmail.com>
2021-01-16 00:12:53 +0200

    Fix some mistakes

--- src/components/PagesConstructor/Nav.js
+++ src/components/PagesConstructor/Nav.js
@@ -4,7 +4,7 @@ import { NavLink, BrowserRouter } from 'react-router-dom';
 export const Nav = () => {
     return (
         <div className='nav-pages'>
-            <BrowserRouter basename="/">
+            <BrowserRouter>
                 <NavLink to='./mainPage' className='nav-link' activeClassName="active">Main</NavLink>
                 <NavLink to='/posts' className='nav-link' activeClassName="active">Posts</NavLink>
                 <NavLink to='/contacts' className='nav-link' activeClassName="active">Contacts</NavLink>
